014c4bfba3cedd93cf87465e5d1a9a34a2e15e73
GUHAN B <guhanreo@gmail.com>
2021-06-13 18:43:03 +0530

    Creating Basic Binary Tree using Linked List

---
+++ BinaryTree/BinaryTreeusingLinkedList/createBinarytree.java
@@ -0,0 +1,27 @@
+package binarytree;
+
+
+
+
+
+public class BinarytreeusingLL {
+    public String value;
+    public BinarytreeusingLL left;
+    public BinarytreeusingLL right;
+    public int height;
+
+    public static class binarytreeLL{
+        BinarytreeusingLL root;
+
+        public binarytreeLL(){
+            this.root = null;
+        }
+        
+
+    public static void main(String[] args) {
+        binarytreeLL BinaryTree = new binarytreeLL();
+        
+        
+    }
+
+}
