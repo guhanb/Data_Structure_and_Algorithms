f7f09c00c8b6176df88dbb9a298b1105c709724e
GUHAN B <guhanreo@gmail.com>
2021-06-15 18:39:53 +0530

    Peek a value in BinaryHeap

---
+++ binaryheap/peek.java
@@ -0,0 +1,44 @@
+package binaryheap;
+
+public class binaryheap {
+    int arr[];
+    int sizeoftree;
+
+
+    public binaryheap(int size){
+        arr = new int[size + 1];
+        this.sizeoftree = 0;
+        System.out.println("The BinaryHeap is created Successfully");
+    }
+
+    public boolean isEmpty(){
+        if(sizeoftree == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    //peek
+    public int peek(){
+        if(isEmpty()){
+            System.out.println("Binary heap is empty");
+            return -1;
+        }
+        return arr[1];
+    }
+
+    //sizeofheap
+    public int sizeofheap(){
+        return sizeoftree;
+    }
+
+    
+    public static void main(String[] args) {
+        binaryheap BHeap = new binaryheap(5);
+        BHeap.peek();
+        
+
+    }
+
+}
